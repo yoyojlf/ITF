8fb2428ff7c8435c200db919cd285077171be9ad
Yoyo DELL Station <jordhan_07@hotmail.com>
2019-10-01 13:50:21 -0300

    se añade el tipo de usuario

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from usuario.models import Usuario, TipoUsuario
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(TipoUsuario)
--- usuario/models.py
+++ usuario/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
+class TipoUsuario(models.Model):
+    name = models.CharField(max_length=50,blank=False)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name;
 
 class Usuario(AbstractUser):
     rut = models.CharField(max_length=11,blank=True)
@@ -12,6 +16,8 @@ class Usuario(AbstractUser):
     bio = models.CharField(max_length=500,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    tipo_usuario = models.ForeignKey(TipoUsuario,null=True,blank=True,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.username
+
