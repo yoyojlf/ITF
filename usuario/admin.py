92f82eb2583a6c1e8f991ba42bc9cd76513e24d8
Yoyo DELL Station <jordhan_07@hotmail.com>
2019-10-11 19:55:35 -0300

    Se añaden los siguientes modelos Pregunta, Evaluacion, Departamento, EvaluacionPregunta, UsuarioEvaluacion

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from usuario.models import Usuario, TipoUsuario
+from usuario.models import Usuario, TipoUsuario, Pregunta, Evaluacion, Departamento, EvaluacionPregunta, UsuarioEvaluacion
 
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(TipoUsuario)
+admin.site.register(Pregunta)
+admin.site.register(Evaluacion)
+admin.site.register(Departamento)
+admin.site.register(EvaluacionPregunta)
+admin.site.register(UsuarioEvaluacion)
+
+
--- usuario/models.py
+++ usuario/models.py
@@ -9,6 +9,14 @@ class TipoUsuario(models.Model):
     def __str__(self):
         return self.name
 
+#Se agrega tabla de departamento
+class Departamento(models.Model):
+    name = models.CharField(max_length=50,blank=True)
+    description = models.CharField(max_length=500,blank=True)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
+
 class Usuario(AbstractUser):
     rut = models.CharField(max_length=11,blank=True)
     birthday = models.DateField(auto_now_add=True,blank=True)
@@ -17,8 +25,38 @@ class Usuario(AbstractUser):
     bio = models.CharField(max_length=500,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    tipo_usuario = models.ForeignKey(TipoUsuario,null=True,blank=True,on_delete=models.CASCADE)
+    tipo_usuario = models.ForeignKey(TipoUsuario,null=True,blank=True,on_delete=models.DO_NOTHING)
+    department = models.ForeignKey(Departamento, null=True, blank=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.username
+
+#Se añade tabla Pregunta
+class Pregunta(models.Model):
+    question = models.CharField(max_length=100,blank=True)
+    max_score = models.FloatField(blank=True)
+    min_score = models.FloatField(blank=True)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.question
+
+#Se añade tabla Evaluacion
+class Evaluacion(models.Model):
+    acum_score = models.FloatField(blank=True)
+    evaluator = models.ForeignKey(Usuario,null=True,blank=True,on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return "Puntaje Acumulado: {0}".format(self.acum_score)
+
+class UsuarioEvaluacion(models.Model):
+    user = models.ForeignKey(Usuario,null=True,blank=True,on_delete=models.DO_NOTHING)
+    evaluation = models.ForeignKey(Evaluacion,null=True,blank=True,on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return "Evaluado: {0} Puntaje Acumulado: {1}".format(self.user.get_full_name(),self.evaluation.acum_score)
+
+class EvaluacionPregunta(models.Model):
+    evaluation = models.ForeignKey(Evaluacion, null=True, blank=True, on_delete=models.DO_NOTHING)
+    question = models.ForeignKey(Pregunta, null=True, blank=True, on_delete=models.DO_NOTHING)
+    score = models.FloatField(blank=True)
+    def __str__(self):
+        return "Pregunta: {0} Puntaje: {1}".format(self.question.question, self.score)
 
